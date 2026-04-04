c647722e37da413e41978b4ba436f230ae219b94
ShivamYadav2030 <shivam203093@gmail.com>
2026-04-04 09:56:17 +0530

    Day2 work completed

--- Day2.py
+++ Day2.py
@@ -10,3 +10,11 @@ for num in arr:
   result.append(total)
 print(*result)
 
+# Using In-place 
+n=int(input("Enter the number of value in array:"))
+arr=list(map(int,input("Enter the number seperated by space:").split()))
+
+for i in range(1,n):
+  arr[i]=arr[i]+arr[i-1]
+  
+print(*arr)
