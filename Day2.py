63fbc0f5a83c28cdda8b1a7b42a9b1f0d582e272
ShivamYadav2030 <shivam203093@gmail.com>
2026-04-05 09:43:38 +0530

    day2

--- Day2.py
+++ Day2.py
@@ -1,14 +1,14 @@
 # Running Sum of 1D Array
 
-n=int(input("Enter the number of value in array:"))
-arr=list(map(int,input("Enter number seperated by space:").split()))
+# n=int(input("Enter the number of value in array:"))
+# arr=list(map(int,input("Enter number seperated by space:").split()))
 
-result=[]
-total=0
-for num in arr:
-  total+=num
-  result.append(total)
-print(*result)
+# result=[]
+# total=0
+# for num in arr:
+#   total+=num
+#   result.append(total)
+# print(*result)
 
 # Using In-place 
 n=int(input("Enter the number of value in array:"))
