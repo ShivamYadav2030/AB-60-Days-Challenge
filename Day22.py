72b406a66a3a0b084bfe08cb12022c7e53b2c775
ShivamYadav2030 <shivam203093@gmail.com>
2026-04-28 22:33:32 +0530

    Day22 work completed

---
+++ Day22.py
@@ -0,0 +1,25 @@
+class MyQueue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+    def push(self, x):
+        self.in_stack.append(x)
+    def pop(self):
+        self.peek()
+        return self.out_stack.pop()
+    def peek(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1]
+    def empty(self):
+        return len(self.in_stack) == 0 and len(self.out_stack) == 0
+
+
+q = MyQueue()
+q.push(10)
+q.push(20)
+q.push(30)
+print("Front:", q.peek()) 
+print("Removed:", q.pop()) 
+print("Empty:", q.empty())
